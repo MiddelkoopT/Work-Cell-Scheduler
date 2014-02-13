cf91b862c96d25c2a2415751f4e5ae0d5977d4b1
Timothy Middelkoop <MiddelkoopT@missouri.edu>
2014-02-13 13:16:10 -0600

    OSSolverService basic test to ensure the OSSolverService binary works.

---
+++ Test/osTest.php
@@ -0,0 +1,91 @@
+<?php
+// Optimization Services Test Copyright 2014 by WebIS Spring 2014 License Apache 2.0
+require_once 'Work-Cell-Scheduler/TDD/validator.php';
+require_once 'Work-Cell-Scheduler/WCS/os.php';
+
+class MyTestCase extends WebIS\Validator {
+
+	protected static $__CLASS__=__CLASS__;
+
+	// Basic OSSolverServices test.
+	function testOS() {
+		exec(WebIS\OS::$solver." --version",$output,$return);
+		//print_r($output);
+		$this->assertEquals(0,$return);
+		$this->assertContains("OS Version: 2.4",$output[5]);
+		
+		file_put_contents("first.osil",self::$osil);
+		exec(WebIS\OS::$solver." -osil first.osil -osrl first.osrl",$output,$return);
+		$this->assertEquals(0,$return);
+		$result=file_get_contents('first.osrl');
+		$this->assertNotEquals(FALSE,$result);
+		$this->assertContains('<var idx="0">0</var>',$result);
+		$this->assertContains('<var idx="1">40</var>',$result);
+		$this->assertContains('<obj idx="-1">-80</obj>',$result);
+		
+		unlink("first.osil");
+		unlink("first.osrl");
+		
+	}
+
+	static $osil =<<<XML
+<?xml version="1.0" encoding="UTF-8"?>
+<osil xmlns="os.optimizationservices.org" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+	xsi:schemaLocation="os.optimizationservices.org http://www.optimizationservices.org/schemas/OSiL.xsd">
+	<instanceHeader>
+		<name>First OSiL Example</name>
+		<source>Timothy Middelkoop</source>
+		<description>
+			min -x -2y
+			s.t.:
+     			x  + y &lt;= 40
+    			2x + y &lt;= 60
+    			x &gt;=0, y &gt;= 0
+    			
+    		Solution -80 @(x=0,y=40)
+    						
+			OSSolverService -osil first.osil -osrl first.osrl
+		</description>
+	</instanceHeader>
+	<instanceData>
+		<variables numberOfVariables="2">
+			<var name="x" />
+			<var name="y" />
+		</variables>
+		<objectives>
+			<obj maxOrMin="min" numberOfObjCoef="2">
+				<coef idx="0">-1</coef>
+				<coef idx="1">-2</coef>
+			</obj>
+		</objectives>
+		<constraints numberOfConstraints="2">
+			<con ub="40"/>
+			<con ub="60"/>
+		</constraints>
+		<linearConstraintCoefficients numberOfValues="4">
+			<start>
+				<el>0</el>
+				<el>2</el>
+				<el>4</el>
+			</start>
+			<colIdx>
+				<el>0</el><el>1</el>
+				<el>0</el><el>1</el>
+			</colIdx>
+			<value>
+				<el>1</el><el>1</el>
+				<el>2</el><el>1</el>			
+			</value>
+		</linearConstraintCoefficients>
+	</instanceData>
+</osil>
+XML;
+
+}
+
+if (!defined('PHPUnit_MAIN_METHOD')) {
+	MyTEstCase::main();
+}
+
+?>
+
---
+++ WCS/os.php
@@ -0,0 +1,9 @@
+<?php
+// Optimization Services Copyright 2014 by WebIS Spring 2014 License Apache 2.0
+namespace WebIS;
+
+class OS {
+	static $solver="\\WebIS\\bin\OSSolverService.exe";
+}
+
+?>
