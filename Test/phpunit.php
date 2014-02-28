251e563149c57c9d87778af02a4934b7fdc6f725
Timothy Middelkoop <MiddelkoopT@missouri.edu>
2014-02-27 18:02:50 -0600

    Run tests in order specified in phpunit to ensure system tests run first

--- Test/phpunit.php
+++ Test/phpunit.php
@@ -1,6 +1,13 @@
 <?php // Unit test entire project.
 require_once 'PHPUnit/Autoload.php';
-$command = new PHPUnit_TextUI_Command;
-return $command->run(array('phpunit.php','../'), TRUE);
+foreach(array('../Test','../TDD','../App') as $dir) {
+	$command = new PHPUnit_TextUI_Command;
+	$ret = $command->run(array('phpunit.php',$dir), FALSE);
+	if($ret){
+		echo "Test $dir failed";
+		exit($ret);
+	}
+}
+exit(0);
 ?>
 
