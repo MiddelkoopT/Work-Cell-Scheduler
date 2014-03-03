321e8136eadfac9f57650db3ad5f60ce5b6d52c3
Timothy Middelkoop <MiddelkoopT@missouri.edu>
2014-03-03 11:47:51 -0600

    Start of class Week 7a.

--- App/personTest.php
+++ App/personTest.php
@@ -9,20 +9,7 @@ class PersonTestCase extends WebIS\Validator {
 	protected static $__CLASS__=__CLASS__;
 
 	function testTrainingApp() {
-		$p=new \WCS\Person();
-		$this->assertTrue($p->setPerson("DrMiddelkoop"));
-		$this->assertFalse($p->setPerson("Dr.Middelkoop"));
-		$this->assertEquals("{person: DrMiddelkoop}",$p->display());
-		$this->assertTrue($p->setName("Dr. Middelkoop"));
-		$this->assertEquals("{person: DrMiddelkoop name: Dr. Middelkoop}",$p->display());
-		$this->assertTrue($p->delete());
-		$this->assertTrue($p->delete()); // second one should also succeed.
-		$this->assertTrue($p->insert());
-		$this->assertFalse($p->insert(),"record exists, should return false");
-		$p=new \WCS\Person();
-		$this->assertTrue($p->setPerson("DrMiddelkoop"));
-		$this->assertTrue($p->get());
-		$this->assertEquals("{person: DrMiddelkoop name: Dr. Middelkoop}",$p->display());
+
 	}
 	
 }
