8cd26f447a52102582cf72864430455f35de7372
Timothy Middelkoop <MiddelkoopT@missouri.edu>
2014-03-05 12:26:30 -0600

    Set and Put data in Person object, connect to database.

--- App/personApp.php
+++ App/personApp.php
@@ -4,7 +4,45 @@ namespace WCS;
 require_once 'Work-Cell-Scheduler/Config/global.php';
 
 class Person {
-
+	private $person=NULL;
+	private $name=NULL;
+	
+	function display(){
+		$str="{person: $this->person";
+		if(!is_null($this->name)){
+			$str.=" name: $this->name";
+		}
+		return $str.'}';
+	}
+	
+	function setPerson($person){
+		if(preg_match('/^[a-zA-Z0-9]+$/',$person)){
+			$this->person=$person;
+			return TRUE;
+		}
+		return FALSE;
+	}
+	
+	function getName(){
+		return $this->name;
+	}
+	
+	function setName($name){
+		if(preg_match('/^\s*$/',$name)){
+			return FALSE;
+		}
+		$this->name=$name;
+		return TRUE;
+	}
+	
+	function write(){
+		$db=new \mysqli('127.0.0.1','root','webis','WCS');
+		if($db===NULL){
+			echo "Person.write:","cannot connect to database";
+		}
+		
+		return TRUE;
+	}
 		
 }
 
--- App/personTest.php
+++ App/personTest.php
@@ -9,7 +9,20 @@ class PersonTestCase extends WebIS\Validator {
 	protected static $__CLASS__=__CLASS__;
 
 	function testTrainingApp() {
-
+		$p=new \WCS\Person();
+		$this->assertTrue($p->setPerson("DrMiddelkoop"));
+		$this->assertFalse($p->setPerson("Dr.Middelkoop"));
+		$this->assertEquals("{person: DrMiddelkoop}",$p->display(),"display only person");
+		$this->assertTrue($p->setName("Dr. Middelkoop"));
+		$this->assertFalse($p->setName(""));
+		$this->assertFalse($p->setName(" "));
+		$this->assertEquals("Dr. Middelkoop",$p->getName());
+		$this->assertEquals("{person: DrMiddelkoop name: Dr. Middelkoop}",$p->display(),"adding name to object");
+		// echo $p->display();
+		$this->assertTrue($p->write());
+		
+		
+		
 	}
 	
 }
