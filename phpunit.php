705d86b34ee406b776d103008c20cff5804ca7b2
Timothy Middelkoop <MiddelkoopT@missouri.edu>
2014-03-07 20:06:32 -0600

    Count the number of passed tests at the end of phpunit.

--- phpunit.php
+++ phpunit.php
@@ -1,14 +1,20 @@
 <?php // Unit test entire project.
 header("Content-type: text/plain");
 require_once 'PHPUnit/Autoload.php';
-foreach(array('TDD','Test','WCS','App') as $dir) {
+echo "Work-Cell-Scheduler TDD\n";
+$tests=array('TDD','Test','WCS','App');
+$passed=0;
+foreach($tests as $dir) {
 	$command = new PHPUnit_TextUI_Command;
 	$ret = $command->run(array('phpunit.php',$dir), FALSE);
 	if($ret){
 		echo "Test $dir failed";
 		exit($ret);
+	}else{
+		$passed+=1;
 	}
 }
-exit(0);
+echo "\nWork-Cell-Scheduler TDD $passed of ",sizeof($tests)," tests passed.\n";
+exit(sizeof($tests)-$passed);
 ?>
 
