58955f2ef9fdcfdfa0ca55b1bb3083acae45276e
Mark Dintelman <mrdr53@mail.missouri.edu>
2014-03-30 20:49:19 -0500

    commit

--- App/ta3test.php
+++ App/ta3test.php
@@ -11,26 +11,43 @@ class WorkersTestCase extends WebIS\Validator {
 	function testworkers(){
 		$w= new \WCS\Workers2();
 		$this->assertTrue($w->SetID("102"));
-		$this->assertFalse($w->SetID("Mark"));
-		$this->assertFalse($w->SetName(" "));
 		$this->assertTrue($w->SetName("Mark Dintelman"));
-		$this->assertTrue($w->Setratesub1("0.9"));
-		$this->assertTrue($w->Setratesub2("0.8"));
-		$this->assertTrue($w->insert());
+		$this->assertTrue($w->SetrateSub1("0.9"));	
+		$this->assertTrue($w->SetrateSub2("0.8"));
 		$this->assertTrue($w->delete());
 		$this->assertTrue($w->insert());
-		$this->assertEquals(array('102'),$w->getworkerID());
-		$this->assertEquals(array('Mark Dintelman'),$w->getname());
-		$this->assertEquals(array('0.9'),$w->getrateSub1());
-		$this->assertEquals(array('0.8'),$w->getrateSub2());
+		$this->assertEquals(("102"),$w->getworkerIDs());
+		$this->assertEquals(("Mark Dintelman"),$w->getnames());
+		
 	
 		
-
-
-
-
-
 	}
+	
+	function testworkerApp(){
+		$p= new \WCS\Workers2();
+		$this->assertTrue($p->SetName("Mark Dintelman"));
+		$a= new \WCS\Workers2App();
+		$this->assertTrue($a->add($p),"Unable to add worker to edit app");
+		$this->assertContains("Mark Dintelman",$a->edit("ta3.php"),"Edit app does not edit");
+		
+		
+		$a=new \WCS\Workers2App();
+		$this->assertFalse($a->load(),"Should not load empty person");
+		$_REQUEST['action']='Load';
+		$_REQUEST['workerID']='102';
+		$this->assertTrue($a->load(),"workerID:102 is not in the database");
+		$this->assertContains("Mark Dintelman",$p->getnames(),"WorkerID object does not contain name");
+		$this->assertEquals(array("0.9"),$p->getrateSub1(),"WorkerID object does not contain name");
+		$_REQUEST['action']='Update';
+		$_REQUEST['workerID']='102';
+		$_REQUEST['name']='Mark Dintelman';
+		$_REQUEST['rateSub1']='0.9';
+		$_REQUEST['rateSub2']='0.8';
+		$this->assertTrue($a->save(),"Cannot save");
+		
+	}
+	
+	
 
 }
 if (!defined('PHPUnit_MAIN_METHOD')) {
