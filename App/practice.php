0fed9708beb487f255406d6bc214a0c0974fb86a
Jianghong <leeeeeego@gmail.com>
2014-04-09 10:01:17 -0500

    practice

--- App/practice.php
+++ App/practice.php
@@ -1,23 +1,19 @@
 <?php
-//Problem 1
-assert(TRUE);
-//assert(FALSE);
+// Problem 1
+assert ( TRUE );
+// assert(FALSE);
 
-//Problem 2
-function tddStringEquals($expected,$result){
-
-	return $expected===$result;
-
+// Problem 2
+function tddStringEquals($expected, $result) {
+	return $expected === $result;
 }
 
 assert(tddStringEquals("one","one"));
 //assert(tddStringEquals("one", "two"));
 
 //Problem 3
-function tddAssertStringEquals($expected,$result){
-
-	assert($expected===$result);
-
+function tddAssertStringEquals($expected, $result) {
+	assert ( $expected === $result );
 }
 tddAssertStringEquals("one", "one");
 //tddAssertStringEquals("one", "two");
@@ -67,12 +63,13 @@ assert($fail);
 
 $XML=<<<XML
  <? xml version="1.0"?>
-<osil><instanceHeader/><osil/>
+<osil><instanceHeader/></osil>
 XML;
 function osil(){
 
 	$osil = new SimpleXMLElement('<osil/>');
-	$osil->addChild('instanceHeader/');
+	//do not need to add one more "/"
+	$osil->addChild('instanceHeader');                                                                                                                       
 	return $osil->asXML();
 
 }
@@ -80,4 +77,30 @@ function osil(){
 
 echo $XML."\n";
 echo osil();
+
+//Problem 6
+
+function assertContainsString($needle,$haystack){
+	//in_array or foreach(strpos)
+	
+	if(in_array($haystack,$needle)===FALSE){
+		
+	    throw new Exception ("assertContainString:|$needle|$haystack");	
+	}
+	
+}
+$fail=FALSE;
+try{assertContainsString(array('Needle','hay','hello'),"Needle");}
+catch(Exception $e){
+	$fail=TRUE;
+}
+assert($fail);
+
+//Problem 7
+
+exec("..\\..\\..\\bin\OSSolverService.exe -v",$output,$result);
+//exec("..\\..\\..\\bin\OSSolverService.exe -h",$output,$result);
+print_r($output);
+print_r($result);
+
 ?>
