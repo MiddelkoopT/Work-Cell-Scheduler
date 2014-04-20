ae75625cb3f8b37013b290e843f3494737661eda
Timothy Middelkoop <MiddelkoopT@missouri.edu>
2014-04-20 14:08:18 -0500

    Split Problem into data and model

--- Web/problemdata.php
+++ Web/problemdata.php
@@ -4,22 +4,14 @@ echo "<html><title>problembasic.php</title><body>\n";
 require_once 'Work-Cell-Scheduler/WCS/os.php';
 require_once 'tdd.php';
 
-// E: Refactor modelbasic.php to problembasic.php.
-// Make the problem a class so that it can be replaced by a Business Object backed by a database at some point.
+// F: Refactor the "data" out of the class into a $data and $problem.
 
-class SimpleProblem {
+class SimpleProblemData {
 
 	// E1
 	private $workers=5;
 	private $cells=4;
 	private $products=6;
-	
-	// E4
-	/**
-	 * Solver
-	 * @var \WebIS\OS
-	 */
-	private $os=NULL;
 	
 	// E1
 	function loadProblem($workers,$cells,$products){
@@ -84,26 +76,44 @@ class SimpleProblem {
 		}
 		$this->required=$required;
 	}
+
+}
+
+
+class ProblemModel {
+
+	/**
+	 * Solver
+	 * @var \WebIS\OS
+	 */
+	private $os=NULL;
+	private $data=NULL;
+	
+	// F
+	function __construct($data){
+		$this->data=$data;
+	}
 	
 	// E5
 	function displayWorkers() {
+		$data=$this->data;
 		$output=array();
 		$solution='';
 		if($this->os){
 			$solution=$this->os->getSolution();
 		}
 		$output[]="<table border='1'><tr><td>$solution\n";
-		foreach($this->cell as $c){
+		foreach($data->cell as $c){
 			$output[]="<th colspan='3'>$c</th>";
 		}
 		$output[]="<th>Total\n";
 		$total=0.0;
-		foreach($this->worker as $w){
+		foreach($data->worker as $w){
 			$output[]="<tr><th>${w}";
 			$hours=0.0;
-			foreach($this->cell as $c){
+			foreach($data->cell as $c){
 				$var="${w}_${c}";
-				$t=$this->training->get($w,$c);
+				$t=$data->training->get($w,$c);
 				if($this->os){
 					$val=$this->os->getVariable($var);
 					$hours+=$val;
@@ -116,7 +126,7 @@ class SimpleProblem {
 			$output[]="<td>$hours</tr>";
 		}
 		$output[]="<tr><th>Total</th>";
-		foreach($this->celltotal as $ct){
+		foreach($data->celltotal as $ct){
 			$output[]="<td colspan='3'>$ct</td>";
 		}
 		$output[]="<td>$total</tr>";
@@ -127,20 +137,21 @@ class SimpleProblem {
 	
 	// E6
 	function displayProblem() {
+		$data=$this->data;
 		$output=array();
 		$output[]="<table border='1'><tr><td>\n";
-		foreach($this->cell as $c){
+		foreach($data->cell as $c){
 			$output[]="<th colspan='1'>$c</th>";
 		}
 		$output[]="<th>Total\n";
-		foreach($this->product as $p){
+		foreach($data->product as $p){
 			$output[]="<tr><th>${p}";
 			$total=0.0;
-			foreach($this->cell as $c){
+			foreach($data->cell as $c){
 				$hours=0.0;
 				$var="${p}_${c}";
-				if(array_key_exists($var,$this->demand)){
-					$d=$this->demand[$var];
+				if(array_key_exists($var,$data->demand)){
+					$d=$data->demand[$var];
 					$hours=$d->hours;
 				}
 				$output[]="<td>$hours";
@@ -150,7 +161,7 @@ class SimpleProblem {
 		}
 		$output[]="<tr><th>Total</th>";
 		$total=0.0;
-		foreach($this->celltotal as $ct){
+		foreach($data->celltotal as $ct){
 			$output[]="<td colspan='1'>$ct</td>";
 			$total+=$ct;
 		}
@@ -164,8 +175,8 @@ class SimpleProblem {
 	function solve(){
 		$os=new \WebIS\OS;
 		
-		$worker=$this->worker;
-		$cell=$this->cell;
+		$worker=$this->data->worker;
+		$cell=$this->data->cell;
 		
 		// Variables and objective function
 		foreach($worker as $w){
@@ -177,8 +188,8 @@ class SimpleProblem {
 		}
 		
 		// Production constraints (use effective hours for productivity)
-		$celltotal=$this->celltotal;
-		$training=$this->training;
+		$celltotal=$this->data->celltotal;
+		$training=$this->data->training;
 		foreach($celltotal as $c=>$lb){
 			$os->addConstraint($lb,NULL);
 			foreach($worker as $w){
@@ -242,33 +253,19 @@ class Training {
 }
 
 
-// E1: Use an empty constructor and use a function call loadProblem() with workers,cells, and products
-// This function generates the list of workers, cells, and products.
-// Store in an associative array with product_cell as key.
+$d=new SimpleProblemData;
+$d->loadProblem(5,4,6);
 
-$p=new SimpleProblem;
-$p->loadProblem(5,4,6);
+$d->loadDemand();
+$d->loadTraining();
+$d->calculate();
 
-// E2: Refactor demand generation as loadDemand()
-$p->loadDemand();
+$m=new ProblemModel($d);
+$m->solve();
+echo $m->displayWorkers();
+echo $m->displayProblem();
 
-// E3: Refactor training generation as loadTraining()
-$p->loadTraining();
-
-// E4: Refactor Calculate values (cell totals, overall total) with calculate();
-$p->calculate();
-
-// E5: Refactor display solution (empty) to verify problem with displayWorkers(); include row and column sum.
-//echo $p->displayWorkers();
-
-// E6: Create a displayProblem function to show product/cell demand, include row and column sum.
-//echo $p->displayProblem();
-
-// E7: Refactor solve() and redisplay
-// echo "<hr>\n";
-$p->solve();
-echo $p->displayWorkers();
-echo $p->displayProblem();
+// G: Replace the generated $data with one supplied by a database $data.
 
 echo "\n</body></html>\n";
 ?>
