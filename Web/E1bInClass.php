1ef920b83f19e2a808a706613ec3f98d7e3e6bc6
Joseph Ahlbrandt <jam84@mail.missouri.edu>
2014-05-12 14:23:01 -0500

    revenue and profit arrays work

--- Web/E1bInClass.php
+++ Web/E1bInClass.php
@@ -14,15 +14,15 @@ $combos = $numSuppliers * $numDepartments;
 for ($i=1; $i<=$numSuppliers; $i++){ //creates suppliers array that contains each supplier
 	$suppliers[]="S$i";
 }
-print "Suppliers\n";
-print_r($suppliers);
+//print "Suppliers\n";
+//print_r($suppliers);
 
 
 for ($i=1; $i<=$numDepartments; $i++){ //creates departments array that contains each department
 	$departments[]="D$i";
 }
 //print "Departments\n";
-print_r($departments);
+//print_r($departments);
 
 
 class Department{ 		//department class gives demand and profit attributes to each department
@@ -59,8 +59,8 @@ $supplierCap["$suppliers[2]"] = new Supplier(200,40);
 $supplierCap["$suppliers[3]"] = new Supplier(500,11);
 
 
-print_r($departmentDem);
-print_r($supplierCap);
+//print_r($departmentDem);
+//print_r($supplierCap);
 
 class Distance{
 
@@ -96,7 +96,7 @@ $distanceMatrix["$suppliers[3]-$departments[3]"]=new Distance(2);
 $distanceMatrix["$suppliers[3]-$departments[4]"]=new Distance(2);
 
 
-print_r($distanceMatrix);
+//print_r($distanceMatrix);
 
 $os=new \WebIS\OS;
 //assertTrue($os->solve());
@@ -110,7 +110,7 @@ foreach($suppliers as $s){
 		$var="${s}-${d}";
 		$os->addVariable($var);
 		$os->addObjCoef($var,$departmentDem[$d]->profit - $distanceMatrix["$var"]->distance - $supplierCap[$s]->pCost);
-		print $departmentDem[$d]->profit - $distanceMatrix["$var"]->distance - $supplierCap[$s]->pCost;
+		//print $departmentDem[$d]->profit - $distanceMatrix["$var"]->distance - $supplierCap[$s]->pCost;
 		print "\n";
 		//print "$var\n";
 
@@ -158,8 +158,50 @@ foreach($supplierCap as $s ){
 	}
 }
 
+
 $os->solve();
 
-print_r($os);
+
+
+$x=0;
+$a=0;
+foreach($supplierCap as $s ){
+	$x+=1;
+	foreach($departments as $key){
+		$currentKey="S$x-${key}";
+		print_r($os->getVariable($currentKey));
+		//print $currentKey;
+		print "\n";
+		$currentRevenue = $os->getVariable($currentKey);
+		$revenue["$suppliers[$a]"]=$revenue["$suppliers[$a]"]+$currentRevenue;
+	}
+	$a+=1;
+}
+
+print_r($revenue);
+
+
+$profit[]=NULL;
+$x=0;
+foreach($departments as $key){
+	$a=0;
+	
+	foreach($supplierCap as $s ){
+		$a+=1;
+		$currentKey="S$a-${key}";
+		//print_r($os->getVariable($currentKey));
+		//print $currentKey;
+		$currentProfit = $os->getVariable($currentKey);
+		$profit["$departments[$x]"]=$profit["$departments[$x]"]+$currentProfit;
+
+	}
+	$x+=1;
+}
+print_r($profit);
+
+//print_r($os->value);
+
+
+
 ?>
 
