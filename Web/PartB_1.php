8e10841ebbc24ec7ae4201581e07aa8ac06f940b
jjby95 <jjby95@EBE3404-PC-12.col.missouri.edu>
2014-05-12 13:30:13 -0500

    making my code safe

--- Web/PartB_1.php
+++ Web/PartB_1.php
@@ -1,5 +1,5 @@
 <?php
-function solveProblem($numSupply,$numDepartments,$capacity,$demand,$profit,$cost){
+function solveProblem($numSupply,$numDepartments,$capacity,$demand,$profit,$cost,$prodCost){
 	$supply=array();
 	$departments=array();
 	for($i = 0; $i < $numSupply; $i++) {
@@ -10,28 +10,35 @@ function solveProblem($numSupply,$numDepartments,$capacity,$demand,$profit,$cost
 	}
 
 	if($capacity===0){
-		$capacity=array(); //(s0,s1,s2)
+		$capacity=array(); //(s0,s1,s2,s3)
 		foreach($supply as $s){
 			$capacity[]=rand(3,6)*100;
 		}		
 	}
 	
+	if($prodCost===0){
+		$prodCost=array(); //(s0,s1,s2,s3)
+		foreach($supply as $s){
+			$prodCost[]=rand(1,5)*10;
+		}
+	}
+	
 	if($demand===0){
-		$demand=array(); //(d0,d1,d2)
+		$demand=array(); //(d0,d1,d2,d3,d4)
 		foreach($departments as $d){
 			$demand[]=rand(1,2)*100;
 		}
 	}
 	
 	if($profit===0){
-		$profit=array(); //(d0,d1,d2)
+		$profit=array(); //(d0,d1,d2,d3,d4)
 		foreach($departments as $d){
 			$profit[]=rand(3,6)*10;
 		}
 	}
 
 	if($cost===0){
-		$cost=array(); //(s0d0,s0d1,s0d2,s1d0,s1d1,s1d2,s2d0,s2d1,s2d2)
+		$cost=array(); //(s0d0,s0d1,s0d2,s0d3,s0d4,s1d0,s1d1,s1d2,s1d3,s1d4,s2d0,s2d1,s2d2,s2d3,s2d4,s3d0,s3d1,s3d2,s3d3,s3d4)
 		foreach($supply as $s){
 			foreach ($departments as $d){
 				$cost[]=rand(1,5);
@@ -85,10 +92,25 @@ function solveProblem($numSupply,$numDepartments,$capacity,$demand,$profit,$cost
 	}
 	//print_r($prof);
 	
+	//associate Production Cost with supplier
+	$prodCost1=array();
+	foreach($supply as $key=>$s){
+		$prodCost1[$s]=$prodCost[$key];
+	}
+	//print_r($prodCost1);
+	
+	$prodCost2=array();
+	foreach($supply as $s){
+		foreach($departments as $d){
+			$prodCost2["{$s}_{$d}"]=$prodCost1[$s];
+		}
+	}
+	//print_r($prodCost2);
+	
 	$oFCoef=array();
 	foreach($supply as $s){
 		foreach($departments as $d){
-			$oFCoef["{$s}_{$d}"]=$prof["{$s}_{$d}"]-$cost1["{$s}_{$d}"];
+			$oFCoef["{$s}_{$d}"]=$prof["{$s}_{$d}"]-$cost1["{$s}_{$d}"]-$prodCost2["{$s}_{$d}"];
 		}
 	}
 	//print_r($oFCoef);
@@ -122,8 +144,40 @@ function solveProblem($numSupply,$numDepartments,$capacity,$demand,$profit,$cost
 		}
 	}
 	
+	//Plants must produce 100 units each constraint
+	foreach($supply as $s){
+		$a->addConstraint(NULL,100);
+		foreach($departments as $d){
+			$a->addConstraintCoef("{$s}_{$d}", 1);
+		}
+	}
+	
+	//echo $a->solve();
 	$solution= $a->solve();
 	//print_r($a);
+	
+	
+	$plantProd=array();
+	foreach($supply as $s){
+		$plantProd[$s]=0;
+	}
+	
+
+	foreach($departments as $d){
+		foreach ($supply as $s){
+			$plantProd[$s] += $a->getVariable("{$s}_{$d}");
+		}
+	}
+	
+	print_r($plantProdCost);
+	$plantProdCost=array();
+	foreach($supply as $s){
+		$plantProdCost[$s]=$prodCost1[$s]*$plantProd[$s];
+	}
+	
+	print_r($plantProdCost);
+	
+	
 ?>	
 
 	<h2>Problem Formulation:</h2>
@@ -300,12 +354,12 @@ echo "Pawprint: JJBY95";
 echo '<br>';
 
 echo '<h1>Part A (given problem):</h1>';
-solveProblem(3, 3, [600,300,200],[600,200,300],[20,30,40],[2,3,3,5,2,4,3,2,8]);
+solveProblem(4, 5, [600,300,200,500],[600,200,300,100,300],[20,30,40,25,25],[2,3,3,3,3,5,2,4,4,2,3,2,8,2,2,3,2,4,2,2],[10,14,40,11]);
+//(s0d0,s0d1,s0d2,s0d3,s0d4,s1d0,s1d1,s1d2,s1d3,s1d4,s2d0,s2d1,s2d2,s2d3,s2d4,s3d0,s3d1,s3d2,s3d3,s3d4)
 
-
-echo '<h1>Part B (random problem):</h1>';
+//echo '<h1>Part B (random problem):</h1>';
 //echo '<br>';
-solveProblem(rand(1,3), rand(1,3), 0,0,0,0);
+//solveProblem(rand(1,3), rand(1,3), 0,0,0,0,0);
 
 
 
