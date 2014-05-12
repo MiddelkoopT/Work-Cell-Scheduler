b4f9794a4cb969b4ff31c0083619572e8e6d5ddc
arlxcf <arlxcf@mail.missouri.edu>
2014-05-12 14:21:43 -0500

    Exam in class Final

--- Web/PartB.php
+++ Web/PartB.php
@@ -238,14 +238,233 @@ for($i=0; $i < $numsuppliers*$numdepartments; $i++){
 for($i=0; $i < $numsuppliers*$numdepartments; $i++){
 	$transCostB[] = rand(2,8);
 }
-SetUpSolve($numsuppliers,$numdepartments,$demandB,$supplyB,$salesB,$transCostB);
+//SetUpSolve($numsuppliers,$numdepartments,$demandB,$supplyB,$salesB,$transCostB);
 
 //D. Display
 
 //See End of Function SetUpSolve
 
+//E. Add Plant
+$demandE=array(600,200,300,100,300);
+$supplyE=array(600,300,200,500);
+$salesE=array(20,20,20,20,30,30,30,30,40,40,40,40,25,25,25,25,25,25,25,25);
+$transCostE=array(2,5,3,3,3,2,2,2,3,4,8,4,3,4,2,2,3,2,2,2);
+
+//SetUpSolveE(4,5,$demandE,$supplyE,$salesE,$transCostE);
+
+function SetUpSolveE($numsuppliers,$numdepartments,$demand,$supply,$sales,$transCost){
+
+	$suppliers=array();
+	$departments=array();
+	for($i=0; $i < $numsuppliers; $i++){
+		$suppliers[] = "s - $i";
+	}
+	for($i=0; $i < $numdepartments; $i++){
+		$departments[] = "d - $i";
+	}
+
+	$fromto=array();
+	foreach($departments as $d){
+		foreach($suppliers as $s){
+			$fromto[]= "($s)_($d)";
+		}
+	}
+
+	$profit=array();
+	foreach($sales as $key=>$s){
+		$profit[$key]=($s-$transCost[$key]);
+	}
+
+	require_once 'Work-Cell-Scheduler/WCS/os.php';
+	$a=new WebIS\OS();
+
+	foreach($fromto as $key=>$ft){
+		$b=$a->addVariable("$ft");
+		$a->addObjCoef("$ft",$profit[$key]);
+	}
+
+	foreach($demand as $demi=>$dem){
+		$a->addConstraint(NULL,$dem);
+		foreach($suppliers as $s){
+			$a->addConstraintCoef("($s)_($departments[$demi])",1);
+		}
+	}
+
+	foreach($supply as $suppi=>$supp){
+		$a->addConstraint($supp,NULL);
+		foreach($departments as $d){
+			$a->addConstraintCoef("($suppliers[$suppi])_($d)",1);
+		}
+	}
+	/// Render HTML table
+	echo "Net Profit: $".$a->solve();
+	
+	foreach($suppliers as $key=>$s){
+		echo "<th>Supplier ($s)\n";
+		echo "Supplies: ".$supply[$key]." items";
+	}
+	foreach($departments as $key=>$d){
+		echo "<tr><th>$d Needs: $demand[$key]</th>";
+		foreach($suppliers as $s) {
+			echo "<td> Sent: ".$a->getVariable(("($s)_($d)"))." items"."</td>";
+		}
+		echo "\n";
+	}
+}
+
+//F.
+$demandF=array(600,200,300,100,300);
+$supplyF=array(600,300,200,500);
+$salesF=array(20,20,20,20,30,30,30,30,40,40,40,40,25,25,25,25,25,25,25,25);
+$transCostF=array(2,5,3,3,3,2,2,2,3,4,8,4,3,4,2,2,3,2,2,2);
+$prodCostF=array(10,14,40,11,10,14,40,11,10,14,40,11,10,14,40,11,10,14,40,11);
+
+//SetUpSolveF(4,5,$demandF,$supplyF,$salesF,$transCostF,$prodCostF);
+
+function SetUpSolveF($numsuppliers,$numdepartments,$demand,$supply,$sales,$transCost,$prodCost){
+
+	$suppliers=array();
+	$departments=array();
+	for($i=0; $i < $numsuppliers; $i++){
+		$suppliers[] = "s - $i";
+	}
+	for($i=0; $i < $numdepartments; $i++){
+		$departments[] = "d - $i";
+	}
+
+	$fromto=array();
+	foreach($departments as $d){
+		foreach($suppliers as $s){
+			$fromto[]= "($s)_($d)";
+		}
+	}
+
+	$profit=array();
+	foreach($sales as $key=>$s){
+		$profit[$key]=($s-$transCost[$key]-$prodCost[$key]);
+	}
+
+	require_once 'Work-Cell-Scheduler/WCS/os.php';
+	$a=new WebIS\OS();
+
+	foreach($fromto as $key=>$ft){
+		$b=$a->addVariable("$ft");
+		$a->addObjCoef("$ft",$profit[$key]);
+	}
+
+	foreach($demand as $demi=>$dem){
+		$a->addConstraint(NULL,$dem);
+		foreach($suppliers as $s){
+			$a->addConstraintCoef("($s)_($departments[$demi])",1);
+		}
+	}
+
+	foreach($supply as $suppi=>$supp){
+		$a->addConstraint($supp,NULL);
+		foreach($departments as $d){
+			$a->addConstraintCoef("($suppliers[$suppi])_($d)",1);
+		}
+	}
+	/// Render HTML table
+	echo "Net Profit: $".$a->solve();
+
+	foreach($suppliers as $key=>$s){
+		echo "<th>Supplier ($s)\n";
+		echo "Supplies: ".$supply[$key]." items";
+	}
+	foreach($departments as $key=>$d){
+		echo "<tr><th>$d Needs: $demand[$key]</th>";
+		foreach($suppliers as $s) {
+			echo "<td> Sent: ".$a->getVariable(("($s)_($d)"))." items"."</td>";
+		}
+		echo "\n";
+	}
+}
+
+//G.
+$demandG=array(600,200,300,100,300);
+$supplyG=array(600,300,200,500);
+$salesG=array(20,20,20,20,30,30,30,30,40,40,40,40,25,25,25,25,25,25,25,25);
+$transCostG=array(2,5,3,3,3,2,2,2,3,4,8,4,3,4,2,2,3,2,2,2);
+$prodCostG=array(10,14,40,11,10,14,40,11,10,14,40,11,10,14,40,11,10,14,40,11);
+
+SetUpSolveG(4,5,$demandG,$supplyG,$salesG,$transCostG,$prodCostG);
+
+function SetUpSolveG($numsuppliers,$numdepartments,$demand,$supply,$sales,$transCost,$prodCost){
+
+	$suppliers=array();
+	$departments=array();
+	$prodMin=array(100,100,100,100);
+	$salesPrice=array(20,30,40,25,25);
+	
+	for($i=0; $i < $numsuppliers; $i++){
+		$suppliers[] = "s - $i";
+	}
+	for($i=0; $i < $numdepartments; $i++){
+		$departments[] = "d - $i";
+	}
+
+	$fromto=array();
+	foreach($departments as $d){
+		foreach($suppliers as $s){
+			$fromto[]= "($s)_($d)";
+		}
+	}
+
+	$profit=array();
+	foreach($sales as $key=>$s){
+		$profit[$key]=($s-$transCost[$key]-$prodCost[$key]);
+	}
+
+	require_once 'Work-Cell-Scheduler/WCS/os.php';
+	$a=new WebIS\OS();
+
+	foreach($fromto as $key=>$ft){
+		$b=$a->addVariable("$ft");
+		$a->addObjCoef("$ft",$profit[$key]);
+	}
+
+	foreach($demand as $demi=>$dem){
+		$a->addConstraint(NULL,$dem);
+		foreach($suppliers as $s){
+			$a->addConstraintCoef("($s)_($departments[$demi])",1);
+		}
+	}
+
+	foreach($supply as $suppi=>$supp){
+		$a->addConstraint($supp,NULL);
+		foreach($departments as $d){
+			$a->addConstraintCoef("($suppliers[$suppi])_($d)",1);
+		}
+	}
+	foreach($prodMin as $pm){
+	$a->addConstraint(NULL,100);
+		foreach($departments as $d){
+			foreach($suppliers as $s){
+				$a->addConstraintCoef("($s)_($d)",1);
+			}
+		}
+	}
+	/// H. Display
+	echo "Net Profit: $".$a->solve();
+
+	foreach($suppliers as $key=>$s){
+		echo "<th>Supplier ($s)\n";
+		echo "Total Prod. Cost: $".$supply[$key]*$prodCost[$key];
+	}
+	foreach($departments as $key=>$d){
+		echo "<tr><th>$d Total Profit: $$demand[$key]$salesPrice[$key]</th>";
+		foreach($suppliers as $s) {
+			echo "<td> Sent: ".$a->getVariable(("($s)_($d)"))." items"."</td>";
+		}
+		echo "\n";
+	}
+	
+}
+
 ?>
 
 </body>
-
 </html>
+
+
