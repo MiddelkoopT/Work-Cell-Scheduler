9b17ffd7b509e672326bd209d7779c85f1e110f3
Joseph Ahlbrandt <jam84@mail.missouri.edu>
2014-05-12 13:21:21 -0500

    (e)-(g) working

--- Web/E1bRandom.php
+++ Web/E1bRandom.php
@@ -13,8 +13,8 @@ include_once 'Work-Cell-Scheduler/WCS/os.php';
 //there are the only number that need to be set (supplier and departments)
 //set them and then run the program
 
-$numSuppliers = 5;
-$numDepartments = 5;
+$numSuppliers = 24;
+$numDepartments = 18;
 $combos = $numSuppliers * $numDepartments;
 
 
